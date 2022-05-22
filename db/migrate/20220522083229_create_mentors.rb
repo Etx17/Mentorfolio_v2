efa86465c64ac157586d9f617adf60a20d296137
Etienne de Dianous <etiennededi@hotmail.fr>
2022-05-22 10:34:25 +0200

    mentor and booking models created

---
+++ db/migrate/20220522083229_create_mentors.rb
@@ -0,0 +1,9 @@
+class CreateMentors < ActiveRecord::Migration[6.1]
+  def change
+    create_table :mentors do |t|
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
