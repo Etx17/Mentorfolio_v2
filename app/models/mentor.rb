5665ca23066b24e0d20f1439daba3f04df4a14de
Etienne de Dianous <etiennededi@hotmail.fr>
2022-05-22 22:16:05 +0200

    Added test user to seed db

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,4 +1,4 @@
 class Mentor < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   has_many :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,4 @@ db.destroy
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# User.create(mail: "test@test.com", password)
