5662582a34d9d0f1e1be13f4ac3692539ffcf717
Etienne de Dianous <etiennededi@hotmail.fr>
2022-05-22 10:23:53 +0200

    ok

--- db/migrate/20220522081725_add_columns_to_users.rb
+++ db/migrate/20220522081725_add_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnsToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :role, :string
+    add_column :users, :role, :string, default: "student"
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :bio, :text
