c7e4b39e64c2da8e2a2cf92915006b3d800a1e50
Etienne de Dianous <etiennededi@hotmail.fr>
2022-05-26 13:42:54 +0200

    new meeting path

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,7 +1,26 @@
 class MeetingsController < ApplicationController
   def new
+    @meeting = Meeting.new
   end
 
+
   def create
+    @user = current_user
+    @meeting = Meeting.new(meeting_params)
+    @meeting.user = @user
+    @meeting.status = 0
+    if @meeting.save
+      flash.success = "You'r request has been sent. A mentor will confirm your request soon. "
+      redirect_to dashboard_path
+    else
+      flash.alert = "An error occured"
+      render :new
+    end
+  end
+
+  private
+
+  def meeting_params
+    params.require(:meeting).permit(:start_date, :status)
   end
 end
