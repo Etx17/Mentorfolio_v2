977b55dccc0d533fc362d0a28c11fc25051dd363
Etienne de Dianous <etiennededi@hotmail.fr>
2022-05-28 17:32:09 +0200

    updated mentormessaged but it's a mess I should not keep it in MVP, they can chat by text, the point is just to make them enter in contact

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,34 @@
 class MessagesController < ApplicationController
+  def index
+    if current_user.role == "student"
+      @messages = Message.where(user: current_user)
+    else
+      @mentor = Mentor.find(user: current_user)
+      @messages = Message.where(mentor: @mentor)
+    end
+  end
+
   def new
+    @mentor = Mentor.find(user: current_user)
+    @meeting = Meeting.find(params[:id])
+    @student = @meeting.user
+    @message = Message.new
+    @message.user = @student
+    @message.mentor = @mentor
+  end
+
+  def create
+    @mentor = Mentor.find(user: current_user)
+    @meeting = Meeting.find(params[:id])
+    @student = @meeting.user
+    @message = Message.new(message_params)
+    if @message.save
+      flash.alert = "Message sent successfully. Your student will be notified."
+      redirect_to dashboard_path
+    else
+      flash.alert = "Sorry, something went wrong. Please try again"
+      render :new
+    end
   end
 
   def edit
@@ -9,5 +38,11 @@ class MessagesController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:user, :mentor, :title, :description)
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,14 +14,16 @@ class PagesController < ApplicationController
   def dashboard
     @user = current_user
     if @user.role == "mentor"
-      @mentor = Mentor.where(user_id: current_user.id)
+      @mentor = Mentor.where(user: current_user)
+      @messages = Message.where(mentor: @mentor)
       start_date = params.fetch(:start_date, Date.today).to_date
-      @meetings = Meeting.where(start_time: start_date.beginning_of_week..start_date.end_of_week)
+      @meetings = Meeting.where(start_time: start_date.beginning_of_week..start_date.end_of_week, mentor: @mentor)
       @pending_meetings = Meeting.where(status: 0)
     else
-      @meetings = Meeting.where(user: @user)
+      @meetings = Meeting.where(user: current_user)
+      @messages = Message.where(user: current_user)
+      @validated_meeting = Meeting.where(user: current_user, status: "confirmed")
     end
-
   end
 
   def profile
